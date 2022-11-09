ae83a15399f09725955fdfce880a603196311742
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2022-11-09 07:36:19 +0330

    Add ScrollToTop.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
 import App from './components/app/App';
+import ScrollToTop from './components/utils/ScrollToTop';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
