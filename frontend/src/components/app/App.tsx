f370e004f2c2e6106ccced616f7e3cc8387e3b2d
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2022-11-16 20:12:14 +0330

    Prepare DescriptionBtn.tsx

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -10,6 +10,7 @@ import { queryClient } from '../../react-query/queryClient';
 import store from './store';
 import { ShoppingCartContextProvider } from '../../context/ShoppingCartContext';
 import { WishlistContextProvider } from '../../context/WishlistContext';
+import DescriptionBtn from '../Description/DescriptionBtn';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <div className='w-full max-w-full min-w-full min-h-screen overflow-hidden'>
               <Navbar />
               <Routes />
+              <DescriptionBtn />
               <Footer />
               <ToastContainer rtl />
               <ReactQueryDevtools />
